ab974e4d278a1b6d731710ca28f0f608410224c5
GrapeColor <grapecolor@users.noreply.github.com>
2020-09-17 15:23:00 +0900

    Change token name of environment

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import KissYou from './kiss_you/index.mjs';
 
 const kissYou = new KissYou();
 
-kissYou.login(process.env['KISS_YOU_TOKEN'])
+kissYou.login(process.env['KISS_DAEMON_TOKEN'])
   .catch(console.error);
